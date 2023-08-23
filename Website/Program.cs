deadebffe393cf62ee4edae90776aba42db537d6
Tony Sarna <hypertek@hotmail.com>
2023-08-22 20:24:45 -0400

    Restructured project and reconfigured GitHub Pages publish process.

---
+++ Website/Program.cs
@@ -0,0 +1,14 @@
+namespace CubScoutsPack1703 {
+	public class Program {
+		public static void Main(string[] args) {
+			var builder = WebApplication.CreateBuilder(args);
+			var app = builder.Build();
+
+			app.UseStaticFiles();
+
+			//app.MapGet("/", () => "Hello World!");
+
+			app.Run();
+		}
+	}
+}
